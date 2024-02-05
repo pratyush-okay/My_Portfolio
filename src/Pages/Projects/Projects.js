e1299fc2637f26598d7df4e2474a6b9158109e78
pratyush-okay <62669803+pratyush-okay@users.noreply.github.com>
2024-02-05 13:00:21 +1100

    2nd commit

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -18,18 +18,18 @@ const Home = () => {
         <div className="skills-container_project">
             <div className="skill-card_project">
                 <ProjectCard title="Comparative Analysis of Abstractive Dialogue Summarisation Techniques"
-                    description="We evaluated abstractive summarization models in NLP, with a focus on dialogue summarising. A Seq2Seq model is
-                    compared to modern transformers such as BART and T5, demonstrating BART’s greater ability to capture dialogue
-                    subtleties. We also created a custom transformer, which did not perform effectively. " />
+                    description="An in-depth study comparing static and dynamic embeddings in dialogue summarization was conducted using the Seq2Seq, BART, and T5 models. 
+                    Achieved significant enhancements, fine-tuning BART for a notable ROUGE-1 score increase from 0.2866 to 0.4215, surpassing a custom Seq2Seq model. 
+                    Identified critical strengths and weaknesses in NLP models, emphasising the difficult balance between computational resources and the precision of context-aware embeddings required for coherent and accurate summaries " />
                 </div>
             <div className="skill-card_project">
                 <ProjectCard title="Image classification of ELPV datasets"
                     description="Classified defects in Solar Cells from ELPV dataset using EigenCell, KNN+SIFT, and MobileNetV2 models where
-                    MobileNetV approach, outperformed excelling in accuracy and robustness"/>
+                    MobileNetV approach, outperformed excelling in accuracy and robustness. MobileNetV2 excels in defect prediction, outperforming KNN and EigenCell. Superior accuracy, feature extraction, and F1 scores validate its precision"/>
                 </div>
             <div className="skill-card_project">
                 <ProjectCard title="My Portfolio Website"
-                    description="Fully React based personal portfolio with animations and styled components"/>
+                    description="This portfolio website is meticulously crafted using React and JavaScript for dynamic functionality. CSS styling and Material UI ensure an aesthetically pleasing design. Hosted on Vercel, the site boasts seamless performance, efficient navigation, and a visually engaging presentation of projects and skills, showcasing my expertise and creativity."/>
                 </div>
             <div className="skill-card_project">
                 <ProjectCard title="Distributed City Survelliance System"
